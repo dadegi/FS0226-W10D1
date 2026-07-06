0e63a224f2c6948eb9d17b6e2a7927843b97a608
Naviganet <dadegi@gmail.com>
2026-07-06 11:22:01 +0200

    Codici lezione W10D1

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,16 +1,5 @@
-import { Alert } from 'react-bootstrap'
+import { Alert } from 'react-bootstrap';
 
-const Detail = (props) => (
-  <Alert variant="success">
-    {props.selectedFromApp || 'Nessun valore selezionato'}
-  </Alert>
-)
+const Details = (props) => {};
 
-export default Detail
-
-// '' è un valore "FALSY"
-// null è un valore "FALSY"
-// undefined è un valore "FALSY"
-// 0 è un valore "FALSY"
-// -0 è un valore "FALSY"
-// false è un valore "FALSY"
+export default Details;
