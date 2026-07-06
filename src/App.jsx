5d8d329ff1c140e89fef0a61bd4e612a08a09e4e
Naviganet <dadegi@gmail.com>
2026-07-06 11:18:29 +0200

    Codici lezione W10D1

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,52 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Col, Container, Row } from 'react-bootstrap'
-import Table from './components/Table'
-import Detail from './components/Detail'
-import { Component } from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from './assets/components/Table';
+// import Details from './assets/components/Details';
+import { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+// Il componente Table dovrà comunicare il proprio stato al componente Details, che sarà fratello nella gerarchia di App. Per definizione i due componenti fratelli non possono comunicare tra loro, quindi, perché condividano lo stesso stato, devo ELEVARLO, cioè lo devo far gestire al componente padre (App) che lo comunicherà a entrambi tramite props.
+// Il component App deve quindi diventare (per ora) un class component
 
 class App extends Component {
-  // Non posso fare in modo che lo stato di Table sia visibile nel componente Detail, ma posso fare
-  // in modo che entrambi condividano lo stesso stato -> devo ELEVARLO nel componente PADRE DI ENTRAMBI
-  // più vicino, e ri-passarlo in basso ad entrambi come PROP
+	// Table avrà come stato la selezione di un elemento di una lista
+	state = {
+		selected: '',
+	};
 
-  // sposto lo stato di Table qui in App
-  state = {
-    selected: '', // ma poi diventerà 'Uno", 'Due' oppure 'Tre'
-  }
+	// Ora questo stato può essere comunicato sia a Table che a Details come prop
+	// Sarà però il click su un elemento di lista in Table che dovrà modificare questo stato, quindi ci serve un metodo che faccia questo
 
-  // ora sono in grado di passare lo stato come prop sia a Table che a Detail -> lo condividono!
-  // ...però Table era anche in grado di modificare lo stato, cliccando sugli elementi di lista...
-  // ...dobbiamo ripristinare questa funzionalità!
+	setAppState = (newValue) => {
+		this.setState({
+			selected: newValue,
+		});
+	};
 
-  // dobbiamo passare a Table this.setState()
-  setAppState = (newValue) => {
-    // tipicamente newValue assume il valore di "Uno", "Due" o "Tre"
-    this.setState({
-      selected: newValue, // "Uno", "Due" o "Tre"
-    })
-  }
+	// L'ideale sarebbe che table invocasse direttamte this.props.setAppState(valore). Per fare questo va passato a Table NON SOLO il valore di state, ma anche un metodo per cambiarlo
 
-  // l'idea sarebbe che Table invocasse -> this.props.setAppState('Due')
-  // dobbiamo passare a Table NON SOLO il "valore" di state, ma anche un modo per "cambiarlo!"
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Container>
-            <Row>
-              <Col>
-                <Table
-                  // LEGGE LO STATO
-                  selectedFromApp={this.state.selected}
-                  // però Table dev'essere messo in grado anche di MODIFICARE lo stato di App...
-                  setAppState={this.setAppState}
-                  // setAppState è una funzione! la passo a Table
-                />
-              </Col>
-              <Col className="my-auto">
-                <Detail
-                  // LEGGE LO STATO
-                  selectedFromApp={this.state.selected}
-                />
-              </Col>
-            </Row>
-          </Container>
-        </header>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<header className='container-fluid bg-dark'>
+					<h1 className='text-center text-light'>STATE ELEVATION</h1>
+				</header>
+				<Container>
+					<Row className='mt-3'>
+						<Col>
+							<Table 
+								// LEGGE lo stato
+								selectedFromApp = {this.state.selected}
+								// Per MODIFICARE lo stato...
+								setAppState = {this.setAppState}
+								// ... passo a Table il metodo setAppState() di riga 20 come PROP
+							/>
+						</Col>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
 
-export default App
-
-// STATE ELEVATION (TL, DR -> Too Long, Didn't Read)
-// lo stato dei componenti non si può condividere tra elementi allo stesso livello (cioè NON
-// in una relazione padre-figlio). L'unico modo per mettere in comunicazione questi componenti "fratelli"
-// è trovare il loro "padre in comune" e spostare lo stato lì; a quel punto potete ripassarlo in bass
-// a tutti i figli che lo vogliono leggere tramite le props
+export default App;
